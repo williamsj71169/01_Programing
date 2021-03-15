226a46514fc0a50c66582388e17d797df5a6902f
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-03-16 09:54:56 +1300

    smnakclists

---
+++ Programing/08_Snack_list_v1.py
@@ -0,0 +1,24 @@
+# Initialise snack lists...
+
+names = ['A', 'B', 'C', 'D', 'E']
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
+
+snack_menu_dict = {
+    'Popcorn': popcorn,
+    'M&Ms': mms,
+    'Pita Chips': pita_chips,
+    'Water': water,
+    'Orange Juice': orange_juice
+    }
+
+test_data = [
+
+    
+]
