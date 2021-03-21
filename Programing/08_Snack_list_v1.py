27f011fc350d14eb2e678a16a07d59f7430986f1
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-03-22 10:01:44 +1300

    5

--- Programing/08_Snack_list_v1.py
+++ Programing/08_Snack_list_v1.py
@@ -36,3 +36,17 @@ for client_order in test_data:
     # get order (hard coded for easy training)
     snack_order = test_data[count]
     count += 1
+
+    for item in snack_order:
+        if len(item) > 0:
+            to_find = (item[1])
+            amount = (item[0])
+            add_list = snack_menu_dict[to_find]
+            add_list[-1] = amount
+
+print()
+print("Popcorn: ", snack_lists[0])
+print("M&Ms: ", snack_lists[1])
+print("Pita Chips: ", snack_lists[2])
+print("Water: ", snack_lists[3])
+print("Orange Juice: ", snack_lists[4])
