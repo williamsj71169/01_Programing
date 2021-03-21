ffc7b7b9efd0615cfd4f6ce950a624aca2f500b3
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2021-03-22 10:24:01 +1300

    snack stuff

--- Programing/08_Snack_list_v1.py
+++ Programing/08_Snack_list_v1.py
@@ -1,6 +1,8 @@
+import pandas
+
 # Initialise snack lists...
 
-names = ['A', 'B', 'C', 'D', 'E']
+all_names = ['A', 'B', 'C', 'D', 'E']
 
 popcorn = []
 mms = []
@@ -10,18 +12,22 @@ orange_juice = []
 
 snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 
-snack_menu_dict = {
+# Data frame dictionary
+movie_data_dict = {
+    'Name': all_names,
     'Popcorn': popcorn,
-    'M&Ms': mms,
-    'Pita Chips': pita_chips,
     'Water': water,
+    'Pita Chips': pita_chips,
+    'M&Ms': mms,
     'Orange Juice': orange_juice
     }
 
 test_data = [
-    [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
-    [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
-    [[1, 'Orange Juice']],
+    [[2, 'Popcorn'], [1, 'Pita Chips'], [1, 'Orange Juice']],
+    [[]],
+    [[1, 'Water']],
+    [[1, 'Popcorn'], [1, 'Orange Juice']],
+    [[1, 'M&Ms'], [1, 'Pita Chips'], [1, 'Orange Juice']],
 ]
 
 count = 0
@@ -31,9 +37,9 @@ for client_order in test_data:
     for item in snack_lists:
         item.append(0)
 
-    # print (snack_lists)
+    # print(snack_lists)
 
-    # get order (hard coded for easy training)
+    # get order (hard coded for easy testing)...
     snack_order = test_data[count]
     count += 1
 
@@ -41,12 +47,18 @@ for client_order in test_data:
         if len(item) > 0:
             to_find = (item[1])
             amount = (item[0])
-            add_list = snack_menu_dict[to_find]
+            add_list = movie_data_dict[to_find]
             add_list[-1] = amount
 
 print()
+print("Names: ", all_names)
 print("Popcorn: ", snack_lists[0])
 print("M&Ms: ", snack_lists[1])
 print("Pita Chips: ", snack_lists[2])
 print("Water: ", snack_lists[3])
 print("Orange Juice: ", snack_lists[4])
+
+# print details...
+movie_frame = pandas.DataFrame(movie_data_dict)
+movie_frame = movie_frame.set_index('Name')
+print(movie_frame)
